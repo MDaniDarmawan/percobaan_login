577ee265e29966fb5f24678ca981d683b75db24e
MDaniDarmawan <darmawandani1922@gmail.com>
2023-10-17 13:52:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/percobaan_login/LoginActivity.kt
@@ -0,0 +1,43 @@
+package com.example.percobaan_login
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.percobaan_login.databinding.FragmentLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    lateinit var email : EditText
+    lateinit var pass: EditText
+    lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        email = binding.email
+        pass = binding.password
+        button = binding.buttonMasuk
+
+        binding.buttonMasuk.setOnClickListener(View.OnClickListener {
+            if (email.text.toString() == "user" && pass.text.toString() == "1234") {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
