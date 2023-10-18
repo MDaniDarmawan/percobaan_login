3eb43b8bdd3eab20fe3dfb27f17bc15b2d9d7e19
MDaniDarmawan <darmawandani1922@gmail.com>
2023-10-18 19:25:05 +0700

    update

--- app/src/main/java/com/example/percobaan_login/LoginActivity.kt
+++ app/src/main/java/com/example/percobaan_login/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.content.Intent
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.percobaan_login.databinding.FragmentLoginBinding
 
@@ -14,22 +15,22 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: FragmentLoginBinding
 
-    lateinit var email : EditText
+   lateinit var email : EditText
     lateinit var pass: EditText
     lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = FragmentLoginBinding.inflate(layoutInflater)
+        /*binding = FragmentLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        email = binding.email
-        pass = binding.password
-        button = binding.buttonMasuk
+        email = binding.etEmail
+        pass = binding.etKataSandi
+        button = binding.tombolMasuk
 
-        binding.buttonMasuk.setOnClickListener(View.OnClickListener {
+        binding.tombolMasuk.setOnClickListener(View.OnClickListener {
             if (email.text.toString() == "user" && pass.text.toString() == "1234") {
                 Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
 
@@ -39,5 +40,10 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
             }
         })
+        binding.pertanyaanDaftar.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
+*/
